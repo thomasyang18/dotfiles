78d0cd515ea7ef0cfb70459a19c1e3ed2af165da
Bobbily King <thomasyang18@github.com>
2025-03-13 18:53:16 -0400

    yay

--- desktop-env/install.sh
+++ desktop-env/install.sh
@@ -1,3 +1,7 @@
 #!/bin/sh 
 
+# yeah this is a giant todo. I still dont know if I'm missing some shit, probably. And plus all of these scripts are hacky since I don't know the proper way of doing things.
+#
+# But hey, it works :D 
+
 echo "Done" # :) 
