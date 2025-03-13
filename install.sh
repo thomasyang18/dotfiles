fa48c8a437054a1f2b4591f7dbee6aad5ebe3bb6
Bobbily King <thomasyang18@github.com>
2025-03-13 14:38:11 -0400

    i hate ai i hate bash i hate ai i hate bash

--- install.sh
+++ install.sh
@@ -3,19 +3,17 @@
 mkdir -p ~/.config
 
 # Get the list of *-env directories
-env_dirs=(*-env)
+env_dirs="$(ls -d *-env)"
 
 # If --dev flag is passed, filter to include only "developer-env"
-if [ "$1" == "--dev" ]; then
-    env_dirs=("developer-env")
+if [ "$1" = "--dev" ]; then
+    env_dirs="developer-env"
 fi
 
 # Loop through filtered list of directories
-for dir in "${env_dirs[@]}"; do
-    if [ -d "$dir" ]; then
-        cd "$dir"
-        echo "install.sh" > .stow-local.ignore
-        ./install.sh
-        stow --adopt -v -t ~ .
-    fi
+for dir in $(echo $env_dirs); do
+	cd "$dir"
+	echo "install.sh" > .stow-local.ignore
+	./install.sh
+	stow --adopt -v -t ~ .
 done
