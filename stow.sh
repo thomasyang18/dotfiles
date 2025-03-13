ea4a2590f64cfe458153a7ab4fbb515f878e2174
Bobbily King <thomasyang18@github.com>
2025-03-13 11:48:55 -0400

    changed images

--- stow.sh
+++ stow.sh
@@ -3,5 +3,5 @@
 # not sure how this works but basically we need to delay writing dot files as long as possible lest we be corrupted by bullshit 
 
 stow --adopt -v -t ~ build_dir/
-git restore . 
+# git restore . 
 
