6f77a5943b6f07ead926463a9c117faec75cdd20
Bobbily King <thomasyang18@github.com>
2025-03-14 01:57:11 -0400

    remove latex

--- developer-env/.config/nvim/lua/plugins/configs/treesitter.lua
+++ developer-env/.config/nvim/lua/plugins/configs/treesitter.lua
@@ -1,7 +1,8 @@
 local options = {
   ensure_installed = { "lua", "vim", "vimdoc",
 	-- make my pretty markdown work pretty pls :3 
-	"markdown", "markdown_inline", "html", "latex", "query",
+	"markdown", "markdown_inline", "html", "query",
+	-- "latex", -- this shit is total ass dont use it, doesnt format nicely at all atm 
 	},
 
   highlight = {
