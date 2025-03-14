9d6ffc80411d9e93d5c925b0db01a896e900ef5e
Bobbily King <thomasyang18@github.com>
2025-03-14 11:40:53 -0400

    Changed code completion to be vscode's (tho mightve removed luasnip idk how important that is for me

--- developer-env/.config/nvim/lua/plugins/configs/cmp.lua
+++ developer-env/.config/nvim/lua/plugins/configs/cmp.lua
@@ -69,41 +69,19 @@ local options = {
   formatting = formatting_style,
 
   mapping = {
-    ["<C-p>"] = cmp.mapping.select_prev_item(),
+	-- we remap the default to vscode; e.g. tab is complete, then ctrl + p n for everything else ig.
+	-- Refer to an earlier version of this source code or NVChad2.0 for how it originally was.
+	["<C-p>"] = cmp.mapping.select_prev_item(),
     ["<C-n>"] = cmp.mapping.select_next_item(),
     ["<C-d>"] = cmp.mapping.scroll_docs(-4),
     ["<C-f>"] = cmp.mapping.scroll_docs(4),
-    ["<C-Space>"] = cmp.mapping.complete(),
     ["<C-e>"] = cmp.mapping.close(),
-    ["<CR>"] = cmp.mapping.confirm {
+    ["<Tab>"] = cmp.mapping.confirm {
       behavior = cmp.ConfirmBehavior.Insert,
       select = true,
     },
-    ["<Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif require("luasnip").expand_or_jumpable() then
-        vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
-      else
-        fallback()
-      end
-    end, {
-      "i",
-      "s",
-    }),
-    ["<S-Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif require("luasnip").jumpable(-1) then
-        vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
-      else
-        fallback()
-      end
-    end, {
-      "i",
-      "s",
-    }),
-  },
+	-- Worried about deleting completion snippetss? But the default vscode way is just better so idk. 
+	},
   sources = {
     { name = "nvim_lsp" },
     { name = "luasnip" },
