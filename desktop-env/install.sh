7ac1140b62c68c65bbbda604693cc7b177d13293
this is all thomasyang18 <nixoser>
2025-03-17 22:52:29 -0400

    btop conf

--- desktop-env/install.sh
+++ desktop-env/install.sh
@@ -4,10 +4,15 @@
 #
 # But hey, it works :D 
 
+mkdir ~/.config
+
 rm -rf ~/.config/sway
 rm -rf ~/.config/wofi
 rm -rf ~/.config/alacritty
 rm -rf ~/.config/mako
 rm -rf ~/.config/hypr
+
+rm -rf ~/.config/btop
+mkdir -p ~/.config/btop # btop generates a themes folder. We don't want to own the folder. For the other ones, we actually do (sway) or we're lazy (alacritty). 
 
 echo "Done" # :) 
